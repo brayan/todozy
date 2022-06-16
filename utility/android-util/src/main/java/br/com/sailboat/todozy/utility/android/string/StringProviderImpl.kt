df7285e6c14565ce1961e0e1039afce85aaf4126
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-06-16 14:44:10 -0300

    Remove class

--- utility/android-util/src/main/java/br/com/sailboat/todozy/utility/android/string/StringProviderImpl.kt
+++
@@ -1,8 +0,0 @@
-package br.com.sailboat.todozy.utility.android.string
-
-import android.content.Context
-
-class StringProviderImpl(private val context: Context): StringProvider {
-
-    override fun getString(stringRes: Int) = context.getString(stringRes)
-}
