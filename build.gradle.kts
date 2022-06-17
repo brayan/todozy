b683dcf7038cf4773d2e86c3ac1f8ca642a86537
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-06-17 20:26:07 -0300

    Add test count on build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 buildscript {
 
     repositories {
@@ -17,6 +20,11 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
+private var testsTotal = 0L
+private var testsSuccess = 0L
+private var testsFailed = 0L
+private var testsSkipped = 0L
+
 allprojects {
     repositories {
         google()
@@ -24,6 +32,49 @@ allprojects {
         maven { url = uri("https://maven.fabric.io/public") }
     }
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+// Test Logging
+    tasks.withType(Test::class) {
+        testLogging {
+            events(
+                TestLogEvent.FAILED,
+                TestLogEvent.PASSED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.STANDARD_OUT,
+            )
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+
+            addTestListener(object : TestListener {
+                override fun beforeSuite(suite: TestDescriptor) {}
+                override fun beforeTest(testDescriptor: TestDescriptor) {}
+                override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+                override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                    if (suite.parent == null) {
+                        println("\nTest result: ${result.resultType}")
+                        println("Test summary: ${result.testCount} tests, " +
+                                "${result.successfulTestCount} succeeded, " +
+                                "${result.failedTestCount} failed, " +
+                                "${result.skippedTestCount} skipped")
+
+                        testsTotal += result.testCount
+                        testsSuccess += result.successfulTestCount
+                        testsFailed += result.failedTestCount
+                        testsSkipped += result.skippedTestCount
+
+                        println("Total: $testsTotal tests, " +
+                                "$testsSuccess succeeded, " +
+                                "$testsFailed failed, " +
+                                "$testsSkipped skipped")
+
+                    }
+                }
+            })
+        }
+    }
 }
 
 tasks.register("clean", Delete::class) {
