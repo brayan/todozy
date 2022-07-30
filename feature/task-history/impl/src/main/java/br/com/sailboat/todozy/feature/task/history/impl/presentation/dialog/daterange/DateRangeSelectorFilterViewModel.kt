83703d1d3a0595f0f340212f8272489bbc24bf32
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-07-30 20:44:59 -0300

    Organize imports

--- feature/task-history/impl/src/main/java/br/com/sailboat/todozy/feature/task/history/impl/presentation/dialog/daterange/DateRangeSelectorFilterViewModel.kt
+++ feature/task-history/impl/src/main/java/br/com/sailboat/todozy/feature/task/history/impl/presentation/dialog/daterange/DateRangeSelectorFilterViewModel.kt
@@ -1,12 +1,16 @@
 package br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange
 
-import br.com.sailboat.todozy.feature.task.history.impl.R
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewAction.OnClickConfirmSelectedDates
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewAction.OnSelectFinalDate
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewAction.OnSelectInitialDate
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewAction.OnStart
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewState.Action.ShowDateCantBeGreaterThanTodayMessage
+import br.com.sailboat.todozy.feature.task.history.impl.presentation.dialog.daterange.DateRangeSelectorFilterViewState.Action.ShowFinalDateCantBeLowerThanFinalDateMessage
 import br.com.sailboat.todozy.utility.android.viewmodel.BaseViewModel
 import br.com.sailboat.todozy.utility.kotlin.extension.clearTime
 import br.com.sailboat.todozy.utility.kotlin.extension.isAfterToday
 import br.com.sailboat.todozy.utility.kotlin.extension.orNewCalendar
 import br.com.sailboat.todozy.utility.kotlin.extension.setFinalTimeToCalendar
-import br.com.sailboat.uicomponent.impl.dialog.MessageDialog
 import java.util.Calendar
 
 internal class DateRangeSelectorFilterViewModel(
@@ -15,19 +19,19 @@ internal class DateRangeSelectorFilterViewModel(
 
     override fun dispatchViewAction(viewAction: DateRangeSelectorFilterViewAction) {
         when (viewAction) {
-            is DateRangeSelectorFilterViewAction.OnStart -> onStart(viewAction)
-            is DateRangeSelectorFilterViewAction.OnSelectInitialDate -> onSelectInitialDate(viewAction)
-            is DateRangeSelectorFilterViewAction.OnSelectFinalDate -> onSelectFinalDate(viewAction)
-            is DateRangeSelectorFilterViewAction.OnClickConfirmSelectedDates -> onClickConfirmSelectedDates()
+            is OnStart -> onStart(viewAction)
+            is OnSelectInitialDate -> onSelectInitialDate(viewAction)
+            is OnSelectFinalDate -> onSelectFinalDate(viewAction)
+            is OnClickConfirmSelectedDates -> onClickConfirmSelectedDates()
         }
     }
 
-    private fun onStart(viewAction: DateRangeSelectorFilterViewAction.OnStart) {
+    private fun onStart(viewAction: OnStart) {
         viewAction.initialDate?.run { viewState.initialDate.value = this }
         viewAction.finalDate?.run { viewState.finalDate.value = this }
     }
 
-    private fun onSelectInitialDate(viewAction: DateRangeSelectorFilterViewAction.OnSelectInitialDate) {
+    private fun onSelectInitialDate(viewAction: OnSelectInitialDate) {
         val initialDate = Calendar.getInstance().apply {
             set(Calendar.YEAR, viewAction.year)
             set(Calendar.MONTH, viewAction.month)
@@ -36,19 +40,20 @@ internal class DateRangeSelectorFilterViewModel(
         }
 
         if (initialDate.isAfterToday()) {
-            viewState.action.value = DateRangeSelectorFilterViewState.Action.ShowDateCantBeGreaterThanTodayMessage
+            viewState.action.value = ShowDateCantBeGreaterThanTodayMessage
             return
         }
 
         if (initialDate.after(viewState.finalDate.value)) {
-            viewState.action.value = DateRangeSelectorFilterViewState.Action.ShowInitialDateCantBeGreaterThanFinalDateMessage
+            viewState.action.value =
+                DateRangeSelectorFilterViewState.Action.ShowInitialDateCantBeGreaterThanFinalDateMessage
             return
         }
 
         viewState.initialDate.value = initialDate
     }
 
-    private fun onSelectFinalDate(viewAction: DateRangeSelectorFilterViewAction.OnSelectFinalDate) {
+    private fun onSelectFinalDate(viewAction: OnSelectFinalDate) {
         val finalDate = Calendar.getInstance().apply {
             set(Calendar.YEAR, viewAction.year)
             set(Calendar.MONTH, viewAction.month)
@@ -57,12 +62,12 @@ internal class DateRangeSelectorFilterViewModel(
         }
 
         if (finalDate.isAfterToday()) {
-            viewState.action.value = DateRangeSelectorFilterViewState.Action.ShowDateCantBeGreaterThanTodayMessage
+            viewState.action.value = ShowDateCantBeGreaterThanTodayMessage
             return
         }
 
         if (finalDate.before(viewState.initialDate.value)) {
-            viewState.action.value = DateRangeSelectorFilterViewState.Action.ShowFinalDateCantBeLowerThanFinalDateMessage
+            viewState.action.value = ShowFinalDateCantBeLowerThanFinalDateMessage
             return
         }
 
