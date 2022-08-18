e1ce78fa243ddd323510db5761872d4ac29c9ee3
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-08-18 09:36:45 -0300

    Remove classes

--- utility/android-util/src/main/java/br/com/sailboat/todozy/utility/android/dialog/dateselector/DateSelectorViewAction.kt
+++
@@ -1,9 +0,0 @@
-package br.com.sailboat.todozy.utility.android.dialog.dateselector
-
-import java.util.Calendar
-
-sealed class DateSelectorViewAction {
-    data class OnStart(
-        val calendar: Calendar?,
-    ) : DateSelectorViewAction()
-}
--- utility/android-util/src/main/java/br/com/sailboat/todozy/utility/android/dialog/dateselector/DateSelectorViewState.kt
+++
@@ -1,8 +0,0 @@
-package br.com.sailboat.todozy.utility.android.dialog.dateselector
-
-import androidx.lifecycle.MutableLiveData
-import java.util.Calendar
-
-class DateSelectorViewState {
-    val calendar = MutableLiveData<Calendar>()
-}
