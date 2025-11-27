96ae0198ab69bb3881ea3e10dfd2ca48a8bb1bda
Brayan Bedritchuk <142108388+ambrayan@users.noreply.github.com>
2025-11-27 14:18:32 -0300

    Fix ktlint

--- ui-component/impl/src/main/java/br/com/sailboat/uicomponent/impl/progress/TaskProgressView.kt
+++ ui-component/impl/src/main/java/br/com/sailboat/uicomponent/impl/progress/TaskProgressView.kt
@@ -11,57 +11,60 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import br.com.sailboat.todozy.domain.model.TaskProgressDay
 import br.com.sailboat.todozy.domain.model.TaskProgressRange
 
-class TaskProgressView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-) : FrameLayout(context, attrs, defStyleAttr) {
-    private val composeView = ComposeView(context).apply {
-        layoutParams =
-            LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT,
-            )
-        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-    }
-    private val daysState = mutableStateOf(emptyList<TaskProgressDay>())
-    private val rangeState = mutableStateOf(TaskProgressRange.LAST_YEAR)
-    private val onRangeSelectedState = mutableStateOf<(TaskProgressRange) -> Unit>({})
-    private val onDayClickState = mutableStateOf<((TaskProgressDay) -> Unit)?>(null)
-    private val loadingState = mutableStateOf(false)
-    private val enableDayDetailsState = mutableStateOf(false)
-
-    init {
-        addView(composeView)
-        composeView.setContent {
-            MaterialTheme {
-                Surface {
-                    TaskProgressContent(
-                        days = daysState.value,
-                        selectedRange = rangeState.value,
-                        onRangeSelected = onRangeSelectedState.value,
-                        onDayClick = onDayClickState.value,
-                        isLoading = loadingState.value,
-                        enableDayDetails = enableDayDetailsState.value,
+class TaskProgressView
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+    ) : FrameLayout(context, attrs, defStyleAttr) {
+        private val composeView =
+            ComposeView(context).apply {
+                layoutParams =
+                    LayoutParams(
+                        LayoutParams.MATCH_PARENT,
+                        LayoutParams.WRAP_CONTENT,
                     )
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            }
+        private val daysState = mutableStateOf(emptyList<TaskProgressDay>())
+        private val rangeState = mutableStateOf(TaskProgressRange.LAST_YEAR)
+        private val onRangeSelectedState = mutableStateOf<(TaskProgressRange) -> Unit>({})
+        private val onDayClickState = mutableStateOf<((TaskProgressDay) -> Unit)?>(null)
+        private val loadingState = mutableStateOf(false)
+        private val enableDayDetailsState = mutableStateOf(false)
+
+        init {
+            addView(composeView)
+            composeView.setContent {
+                MaterialTheme {
+                    Surface {
+                        TaskProgressContent(
+                            days = daysState.value,
+                            selectedRange = rangeState.value,
+                            onRangeSelected = onRangeSelectedState.value,
+                            onDayClick = onDayClickState.value,
+                            isLoading = loadingState.value,
+                            enableDayDetails = enableDayDetailsState.value,
+                        )
+                    }
                 }
             }
         }
-    }
 
-    fun render(
-        days: List<TaskProgressDay>,
-        range: TaskProgressRange,
-        onRangeSelected: (TaskProgressRange) -> Unit,
-        onDayClick: ((TaskProgressDay) -> Unit)? = null,
-        isLoading: Boolean = false,
-        enableDayDetails: Boolean = false,
-    ) {
-        onRangeSelectedState.value = onRangeSelected
-        onDayClickState.value = onDayClick
-        daysState.value = days
-        rangeState.value = range
-        loadingState.value = isLoading
-        enableDayDetailsState.value = enableDayDetails
+        fun render(
+            days: List<TaskProgressDay>,
+            range: TaskProgressRange,
+            onRangeSelected: (TaskProgressRange) -> Unit,
+            onDayClick: ((TaskProgressDay) -> Unit)? = null,
+            isLoading: Boolean = false,
+            enableDayDetails: Boolean = false,
+        ) {
+            onRangeSelectedState.value = onRangeSelected
+            onDayClickState.value = onDayClick
+            daysState.value = days
+            rangeState.value = range
+            loadingState.value = isLoading
+            enableDayDetailsState.value = enableDayDetails
+        }
     }
-}
